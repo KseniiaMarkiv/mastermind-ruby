5708ad0032c2077e5f389b1a6acce6601316f867
KseniiaMarkiv <ksenya.markiv@gmail.com>
2024-07-15 16:49:51 -0400

    Initialize game board

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-#Project: Mastermind
+#Project: Mastermind - was performed only on Ruby
 
 _it’s a game where you have to guess your opponent’s secret code within a certain number of turns (like hangman with colored pegs). Each turn you get some feedback about how good your guess was – whether it was exactly correct or just the correct color but in the wrong space._
 
--- mastermind-two-pl-ruby.rb
+++ mastermind-two-pl-ruby.rb
@@ -31,3 +31,18 @@ end
 # variables
 @sequence_colors = red_color('Red, Blue, Green, Yellow, Black, White, Gray, Orange, Purple, Pink')
 @highlight_hint = red_color('hints')
+
+# game conditions
+
+def game_conditions
+  puts red_color("#{@aim_symbol} THE MAIN GOAL #{@aim_symbol} is you have 12 turns to guess 4 colors.")
+  puts "You have only 10 various colors: #{@sequence_colors}"
+  puts "Game will give you #{@highlight_hint} of what color you guessed."
+end
+
+# initialize board for storing moves and colors
+def initialize_board
+  board = Array.new(12, Array.new(4, ''))
+  board[0] = %w(green yellow red blue)
+  board
+end
