45bbfd423b733dcf03ba784a129e9c7db887f7f4
KseniiaMarkiv <ksenya.markiv@gmail.com>
2024-07-15 16:47:59 -0400

    Setup all symbols and variables

--- mastermind-two-pl-ruby.rb
+++ mastermind-two-pl-ruby.rb
@@ -20,3 +20,14 @@ end
 def gray_color(string)
   string.colorize(:gray)
 end
+
+# all symbols
+@celebration_symbol = "\u{1F389}"
+@rainbow_symbol = "\u{1F308}"
+@cool_emoji = "\u{1F60E}"
+@mushroom_emoji = "\u{1F344}"
+@aim_symbol = "\u{1F3AF}"
+
+# variables
+@sequence_colors = red_color('Red, Blue, Green, Yellow, Black, White, Gray, Orange, Purple, Pink')
+@highlight_hint = red_color('hints')
