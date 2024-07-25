5fb0471f1051c02396689e1e95cd9783af15fff6
KseniiaMarkiv <ksenya.markiv@gmail.com>
2024-07-25 12:26:07 -0400

    Added game conditions and initialize board

--- mastermind-ruby.rb
+++ mastermind-ruby.rb
@@ -30,3 +30,18 @@ AIM_SYMBOL = "\u{1F3AF}"
 
 SEQUENCE_COLORS = red_color('Red, Blue, Green, Yellow, Black, White, Gray, Orange, Purple, Pink'.downcase)
 HIGHLIGHT_HINT = red_color('hints')
+
+# game conditions
+def game_conditions
+  puts red_color("#{AIM_SYMBOL} THE MAIN GOAL #{AIM_SYMBOL} is you have 12 turns to guess 4 colors.")
+  puts "You have only 10 various colors: #{SEQUENCE_COLORS}"
+  puts "Game will give you #{HIGHLIGHT_HINT} of what color you guessed."
+end
+
+# initialize board for storing moves and default colors
+def initialize_board
+  board = Array.new(12, Array.new(4, ''))
+  board[0] = %w(green yellow red blue)
+  board
+end
+
